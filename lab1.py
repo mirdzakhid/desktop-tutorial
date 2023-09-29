99b3a442aa341a19f85544551f40644102c077d7
mirdzakhid <mirdzakhid@mail.ru>
2023-09-29 21:27:16 +0300

    start lab v1
    Самая лабораторная(начальная форма)

---
+++ lab1.py
@@ -0,0 +1,19 @@
+a = float(input("Введите первое число: "))
+b = float(input("Введите второе число: "))
+
+sum_ = a + b
+print("Сумма чисел:", sum_)
+
+diff = a - b
+print("Разность чисел:", diff)
+
+product = a * b
+print("Произведение чисел:", product)
+
+average = (a + b) / 2
+print("Среднее арифметическое чисел:", average)
+
+max_abs = max(abs(a), abs(b))
+min_abs = min(abs(a), abs(b))
+abs_diff = max_abs - min_abs
+print("Разность максимального и минимального по модулю:", abs_diff)
