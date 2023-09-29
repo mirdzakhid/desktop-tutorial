03515ac462a29857769165aa9d4c52cb3cc29eba
mirdzakhid <mirdzakhid@mail.ru>
2023-09-29 21:39:58 +0300

    вывод частного введенных чисел с точность до сотых

--- lab1.py
+++ lab1.py
@@ -17,3 +17,6 @@ max_abs = max(abs(a), abs(b))
 min_abs = min(abs(a), abs(b))
 abs_diff = max_abs - min_abs
 print("Разность максимального и минимального по модулю:", abs_diff)
+
+quotient = round(a / b, 2)
+print("Частное чисел:", quotient)
